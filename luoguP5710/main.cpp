867ea61838b99e321fd72acabea0a2805fa2fd96
Joseph Lee <mahironya@icloud.com>
2024-10-13 15:17:00 +0800

    k

---
+++ luoguP5710/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  luoguP5710
+//
+//  Created by Joseph Lee on 10/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
